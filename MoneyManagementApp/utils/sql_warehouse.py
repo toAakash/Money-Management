17fe9599768857809e31716977a83aa7a68fa7a9
Aakash <iaakashec@gmail.com>
2026-02-08 16:20:51 +0530

    test Vs code

--- MoneyManagementApp/utils/sql_warehouse.py
+++ MoneyManagementApp/utils/sql_warehouse.py
@@ -12,3 +12,14 @@ def get_conn():
     )
 
 
+def get_connection():
+
+    cfg = Config()  # Pull environment variables for auth
+    return sql.connect(
+        server_hostname=cfg.host,
+        http_path=f"/sql/1.0/warehouses/{os.getenv('DATABRICKS_WAREHOUSE_ID')}",
+        credentials_provider=lambda: cfg.authenticate
+    )
+
+
+
